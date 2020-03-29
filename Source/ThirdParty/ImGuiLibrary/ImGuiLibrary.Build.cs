e39b615ea1c93d29080a5d59f9c07b6205385f81
Roberto “ResaloliPT” Pires <roberto_pires97@sapo.pt>
2020-03-29 23:59:53 +0000

    Added ImGui 1.75

---
+++ Source/ThirdParty/ImGuiLibrary/ImGuiLibrary.Build.cs
@@ -0,0 +1,9 @@
+using UnrealBuildTool;
+
+public class ImGuiLibrary : ModuleRules
+{
+        public ImGuiLibrary(ReadOnlyTargetRules Target) : base(Target)
+	{
+		Type = ModuleType.External;
+	}
+}
